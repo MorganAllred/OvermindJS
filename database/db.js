dda92c8ed0482ee9dfda6dca44920a378153aeb8
Morgan Allred <mallred@fluenthome.com>
2022-06-14 08:47:37 -0600

    started on the database

---
+++ database/db.js
@@ -0,0 +1,9 @@
+// const mysql = require('mysql2');
+// const { mysqlConnectionInfo } = require('../config.json');
+
+
+// const c = mysql.createConnection(mysqlConnectionInfo);
+
+// c.query('SELECT * FROM Player', (err, results, fields) => {
+// 	console.log(results);
+// });
--- deploy-commands.js
+++ deploy-commands.js
@@ -16,6 +16,11 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+try {
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);
+}
+catch (error) {
+	console.error(error);
+}
