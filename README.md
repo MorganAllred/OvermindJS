581536aced69fb9a809ef7afbfcc3aa5b8720650
Morgan Allred <mallred@fluenthome.com>
2022-07-17 12:01:05 -0600

    added assets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OvermindJS
+*A mastermind robot to make life better*
+
+
+
+## $help
+![displayed help command](https://github.com/MorganAllred/EQ_Minimal/blob/main/Assets/Images/ResourceWindow.PNG?raw=true)
--- database/db.js
+++
@@ -1,9 +0,0 @@
-// const mysql = require('mysql2');
-// const { mysqlConnectionInfo } = require('../config.json');
-
-
-// const c = mysql.createConnection(mysqlConnectionInfo);
-
-// c.query('SELECT * FROM Player', (err, results, fields) => {
-// 	console.log(results);
-// });
