926b619b379358a2091125446acfd6102660ffc4
Morgan Allred <MorganAllred@users.noreply.github.com>
2023-04-16 21:18:34 -0600

    delete useless shit

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-const { generalChannelId, applyChannelId } = require('../config.json');
-
-module.exports = {
-	name: 'guildMemberAdd',
-	execute(member) {
-		member.send(`Hey there! Head over to <#${generalChannelId}> and say hi!\nIf you want to apply, do so here <#${applyChannelId}>`);
-	},
-};
